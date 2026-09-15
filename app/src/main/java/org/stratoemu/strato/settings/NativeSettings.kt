38700b10c98e81e922e85785c346257e60b91297
Hieishadow <104704123+Hieishadow@users.noreply.github.com>
2026-09-15 04:23:27 -0300

    Refactor NativeSettings and add resolutionScale
    Removed comments and updated NativeSettings data class to include resolutionScale.

--- app/src/main/java/org/stratoemu/strato/settings/NativeSettings.kt
+++ app/src/main/java/org/stratoemu/strato/settings/NativeSettings.kt
@@ -1,8 +1,3 @@
-/*
- * SPDX-License-Identifier: MPL-2.0
- * Copyright © 2022 Skyline Team and Contributors (https://github.com/skyline-emu/)
- */
-
 package org.stratoemu.strato.settings
 
 import android.content.Context
@@ -10,24 +5,16 @@ import org.stratoemu.strato.BuildConfig
 import org.stratoemu.strato.utils.GpuDriverHelper
 import kotlinx.serialization.Serializable
 
-/**
- * The settings that will be passed to libskyline when running and executable
- */
 @Serializable
 @Suppress("unused")
 data class NativeSettings(
-    // System
     var isDocked : Boolean,
     var usernameValue : String,
     var profilePictureValue : String,
     var systemLanguage : Int,
     var systemRegion : Int,
     var isInternetEnabled : Boolean,
-
-    // Audio
     var isAudioOutputDisabled : Boolean,
-
-    // GPU
     var gpuDriver : String,
     var gpuDriverLibraryName : String,
     var forceTripleBuffering : Boolean,
@@ -38,13 +25,10 @@ data class NativeSettings(
     var forceMaxGpuClocks : Boolean,
     var freeGuestTextureMemory : Boolean,
     var disableShaderCache : Boolean,
-
-    // Hacks
+    var resolutionScale : Int,
     var enableFastGpuReadbackHack : Boolean,
     var enableFastReadbackWrites : Boolean,
     var disableSubgroupShuffle : Boolean,
-
-    // Debug
     var logLevel : Int,
     var validationLayer : Boolean
 ) {
@@ -66,15 +50,12 @@ data class NativeSettings(
         pref.forceMaxGpuClocks,
         pref.freeGuestTextureMemory,
         pref.disableShaderCache,
+        pref.resolutionScale,
         pref.enableFastGpuReadbackHack,
         pref.enableFastReadbackWrites,
         pref.disableSubgroupShuffle,
         pref.logLevel,
         BuildConfig.BUILD_TYPE != "release" && pref.validationLayer
     )
-
-    /**
-     * Updates settings in libskyline during emulation
-     */
     external fun updateNative()
 }
