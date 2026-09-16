ecb89a2a2dc038ca8df66e486b1c35f111e72532
Hieishadow <104704123+Hieishadow@users.noreply.github.com>
2026-09-16 01:40:23 -0300

    Refactor NvDecClass and update includes

--- app/src/main/cpp/skyline/soc/host1x/classes/nvdec.h
+++ app/src/main/cpp/skyline/soc/host1x/classes/nvdec.h
@@ -1,14 +1,20 @@
-// SPDX-License-Identifier: MPL-2.0
-#include "nvdec.h"
-#include "logger.h"
+#pragma once
+#include <common.h>
+#include <soc/host1x/syncpoint.h>
+
+namespace skyline {
+    namespace soc { namespace host1x { class Host1xDevice; } }
+    struct DeviceState;
+}
 
 namespace skyline::soc::host1x {
-    NvDecClass::NvDecClass(std::function<void()> opDoneCallback)
-        : opDoneCallback(std::move(opDoneCallback)) {}
-
-    void NvDecClass::CallMethod(u32 method, u32 argument) {
-        // S20 FE 865 - pula cutscene preta
-        if (opDoneCallback)
-            opDoneCallback();
-    }
+    class NvDecClass {
+      private:
+        DeviceState &state;
+        SyncpointSet &syncpoints;
+        std::function<void()> opDoneCallback;
+      public:
+        NvDecClass(DeviceState &state, SyncpointSet &syncpoints, std::function<void()> opDoneCallback);
+        void CallMethod(u32 method, u32 argument);
+    };
 }
