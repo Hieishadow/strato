4dccf2e3ea2ccc23c2e50aa6bfdaf6552a10cc63
Hieishadow <104704123+Hieishadow@users.noreply.github.com>
2026-09-16 01:29:24 -0300

    Refactor NvDecClass by removing unnecessary code
    Removed unused includes and member variables from NvDecClass.

--- app/src/main/cpp/skyline/soc/host1x/classes/nvdec.h
+++ app/src/main/cpp/skyline/soc/host1x/classes/nvdec.h
@@ -1,19 +1,12 @@
-// SPDX-License-Identifier: MPL-2.0
 #pragma once
-
 #include <common.h>
-#include <media/NdkMediaCodec.h>
 
 namespace skyline::soc::host1x {
     class NvDecClass {
       private:
         std::function<void()> opDoneCallback;
-        AMediaCodec* decoder = nullptr; // decoder do S20 FE
-        bool isInitialized = false;
-
       public:
         NvDecClass(std::function<void()> opDoneCallback);
         void CallMethod(u32 method, u32 argument);
-        void InitDecoder(); // inicia o MediaCodec
     };
 }
